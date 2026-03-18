703a8ca6f3444ad16bf51ab6df5d5e82469673fd
Clémence <cmacquartmoulin@gmail.com>
2026-03-18 14:35:36 +0100

    actualisation

--- src/models/cnn.py
+++ src/models/cnn.py
@@ -2,23 +2,23 @@ import torch.nn as nn
 from hydra.utils import instantiate
 from omegaconf import DictConfig
 
-def CNN(self):
+class CNN(nn.Module):
     def __init__(self, cfg: DictConfig):
         super().__init__()
 
         conv_layers = []
-        in_channels = cfg.model.conv.in_channels               # nombre de channels à l'entrée
-        for out in cfg.model.conv.out_channels:  # parcourt la liste des tailles des différents canaux
+        in_channels = cfg.model.cnn.in_channels               # nombre de channels à l'entrée
+        for out in cfg.model.cnn.out_channels:  # parcourt la liste des tailles des différents canaux
           conv_layers.append(
           nn.Conv2d(
                   in_channels=in_channels,
                   out_channels=out,
-                  kernel_size=cfg.kernel_size,
-                  stride=cfg.stride,
-                  padding=cfg.padding
+                  kernel_size=cfg.model.cnn.kernel_size,  
+                  stride=cfg.model.cnn.stride,           
+                  padding=cfg.model.cnn.padding 
                 ))
           conv_layers.append(instantiate(cfg.cnn_activation.activation)) # activation du CNN
-          conv_layers.append(nn.MaxPool2d(cfg.model.conv.pool_size))
+          conv_layers.append(nn.MaxPool2d(cfg.model.cnn.pool))
           in_channels = out
         self.cnn = nn.Sequential(*conv_layers)
         
